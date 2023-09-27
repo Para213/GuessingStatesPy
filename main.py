15dbdaa0c93a2d9215ac12287e3da7b3a8b08498
Para213 <94556518+Para213@users.noreply.github.com>
2023-09-27 16:46:09 +0300

    Guessing states game

---
+++ main.py
@@ -0,0 +1,38 @@
+import turtle
+
+import pandas as pd
+from turtle import Turtle
+
+cursor = Turtle()
+cursor.penup()
+cursor.hideturtle()
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+corecte = 0
+frame = pd.read_csv("50_states.csv")
+all_states = frame.state.to_list()
+state_ramase = []
+guessed = []
+
+while corecte != 50:
+    answer_state = screen.textinput(title=f"{corecte}/50 State corecte", prompt="Ghiceste alt stat")
+    answer_state = answer_state.title()
+    if answer_state in all_states and answer_state not in guessed:
+        corecte += 1
+        state_data = frame[frame.state == answer_state]
+        cursor.goto(int(state_data.x), int(state_data.y))
+        cursor.write(state_data.state.item())
+        guessed.append(answer_state)
+    elif answer_state == "Exit":
+        break
+
+for state in all_states:
+    if state not in guessed:
+        state_ramase.append(state)
+new_data = pd.DataFrame(state_ramase)
+new_data.to_csv("gresite.csv")
